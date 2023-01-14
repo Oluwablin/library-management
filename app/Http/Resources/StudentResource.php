2df606a947af552d3478ffb2ea47f226ade37dbe
oluwablin <oyelekeseun@outlook.com>
2023-01-14 10:49:53 +0100

    route for all endpoints

---
+++ app/Http/Resources/StudentResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $data = [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'last_login_at' => $this->last_login_at?->toDayDateTimeString(),
+            'is_active' => ($this->is_active) ? true : false,
+            'avatar' => $this->avatar,
+            'email_verified' => ($this->email_verified_at) ? true : false,
+            'library' => new LibraryResource($this->library),
+            'roles' => RoleResource::collection($this->roles),
+        ];
+
+        return $data;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\v1\API\Auth\AuthController;
+use App\Http\Controllers\v1\API\Library\LibraryController;
+use App\Http\Controllers\v1\API\Record\RecordController;
+use App\Http\Controllers\v1\API\Student\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +35,37 @@ Route::prefix('v1')->group(function () {
     | Here are all superadmin API routes
     */
 
+    // All routes for authenticated super admins should go down here under this group
+    Route::prefix('superadmin')->middleware(['auth:sanctum', 'role:SuperAdmin'])->group(function () {
+
+        // Record endpoints
+        Route::prefix('records')->name('records.')->controller(RecordController::class)->group(function () {
+            Route::get('/{id}', 'getRecord')->name('get-one');
+            Route::get('/', 'getAllRecords')->name('get-all');
+            Route::post('/', 'addNewRecord')->name('add-new');
+            Route::put('/{id}', 'updateRecord')->name('update-one');
+            Route::delete('/{id}', 'deleteRecord')->name('delete-one');
+        });
+
+        // Library endpoints
+        Route::prefix('libraries')->name('libraries.')->controller(LibraryController::class)->group(function () {
+            Route::get('/{id}', 'getLibrary')->name('get-one');
+            Route::get('/', 'getAllLibraries')->name('get-all');
+            Route::post('/', 'addNewLibrary')->name('add-new');
+            Route::put('/{id}', 'updateLibrary')->name('update-one');
+            Route::delete('/{id}', 'deleteLibrary')->name('delete-one');
+        });
+
+        // Student endpoints
+        Route::prefix('students')->name('students.')->controller(StudentController::class)->group(function () {
+            Route::get('/{id}', 'getStudent')->name('get-one');
+            Route::get('/', 'getAllStudents')->name('get-all');
+            Route::post('/', 'addNewStudent')->name('add-new');
+            Route::put('/{id}', 'updateStudent')->name('update-one');
+            Route::delete('/{id}', 'deleteStudent')->name('delete-one');
+        });
+    });
+
     /*
     |--------------------------------------------------------------------------
     | Admin Routes
@@ -39,11 +73,50 @@ Route::prefix('v1')->group(function () {
     | Here are all admins API routes
     */
 
+    // All routes for authenticated admins should go down here under this group
+    Route::prefix('admin')->middleware(['auth:sanctum', 'role:Admin'])->group(function () {
+
+        // Record endpoints
+        Route::prefix('records')->name('records.')->controller(RecordController::class)->group(function () {
+            Route::get('/{id}', 'getParticularRecord')->name('get-particular-one');
+            Route::post('/', 'addParticularRecord')->name('add-particular-one');
+            Route::put('/{id}', 'updateParticularRecord')->name('update-particular-one');
+            Route::delete('/{id}', 'deleteParticularRecord')->name('delete-particular-one');
+        });
+
+        // Library endpoints
+        Route::prefix('libraries')->name('libraries.')->controller(LibraryController::class)->group(function () {
+            Route::get('/{id}', 'getParticularLibrary')->name('get-particular-one');
+        });
+
+        // Student endpoints
+        Route::prefix('students')->name('students.')->controller(StudentController::class)->group(function () {
+            Route::get('/{id}', 'getParticularStudent')->name('get-particular-one');
+            Route::post('/', 'addParticularStudent')->name('add-particular-one');
+            Route::put('/{id}', 'updateParticularStudent')->name('update-particular-one');
+            Route::delete('/{id}', 'deleteParticularStudent')->name('delete-particular-one');
+        });
+    });
+
     /*
     |--------------------------------------------------------------------------
     | Student Routes
     |--------------------------------------------------------------------------
     | Here are all students API routes
     */
+
+    // All routes for authenticated students should go down here under this group
+    Route::prefix('student')->middleware(['auth:sanctum', 'role:Student'])->group(function () {
+
+        // Record endpoints
+        Route::prefix('records')->name('records.')->controller(RecordController::class)->group(function () {
+            Route::get('/{id}', 'getParticularRecord')->name('get-particular-one');
+        });
+
+        // Library endpoints
+        Route::prefix('libraries')->name('libraries.')->controller(LibraryController::class)->group(function () {
+            Route::get('/{id}', 'getParticularLibrary')->name('get-particular-one');
+        });
+    });
 
 });
